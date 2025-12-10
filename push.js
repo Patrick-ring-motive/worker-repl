c89312c0dabf7d7c0b27f2d01e3534f98b80ea3a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-12-09 18:22:17 -0600

    Update push.js

--- push.js
+++ push.js
@@ -1,1 +1,14 @@
 
+
+(() => {
+  if (typeof Window !== 'undefined') return;
+  
+  self.addEventListener('install', event => self.skipWaiting());
+  self.addEventListener('activate', event => self.clients.claim());
+  
+  let userCode = '';
+  
+  self.addEventListener('fetch', event => {
+      console.log(event);
+  });
+})();
